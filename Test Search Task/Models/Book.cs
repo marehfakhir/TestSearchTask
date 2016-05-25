3007e4a52e91ac63c5e0460c384f83cebd21006d
Mareh Al-Sammarraie <Mareh Al-Sammarraie>
2016-05-25 13:33:19 -0400

    code clean up

--- Test Search Task/Models/Book.cs
+++ Test Search Task/Models/Book.cs
@@ -22,17 +22,12 @@ namespace TestSearchTask.Models
         [Display(Name = "New Release: Coming Soon")]
         [DataType(DataType.Date)]
         [CheckFutureDate]
-        //// [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        ////public string ReleaseDate2 { get; set; }
         public DateTime NewReleaseComingSoon { get; set; }
 
         [Display(Name = "New Release: Last 30 Days")]
         [DataType(DataType.Date)]
-        //// [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        //public string ReleaseDate3 { get; set; }
         public DateTime NewReleaseWithinPastMonth { get; set; }
 
-        //  [Display(Name = "Category")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [Required]
         [StringLength(30)]
