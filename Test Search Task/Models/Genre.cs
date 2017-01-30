c565cfb9d005caea5fde816c4b80f83b32e2ef8f
marah.fakher@gmail.com <marah.fakher@gmail.com>
2017-01-29 21:43:28 -0500

    Added missing validation files + Missing model class

---
+++ Test Search Task/Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace Test_Search_Task.Models
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class GenreDBContext : DbContext
+    {
+        public DbSet<Genre> GenreTypes { get; set; }
+    }
+}
---
+++ Test Search Task/Validation/CheckPastDateAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test_Search_Task.Validation
+{
+    public class CheckPastDateAttribute : ValidationAttribute
+    {
+        public CheckPastDateAttribute()
+            : base("{0} must be within the past 30 days.")
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && (DateTime)value != DateTime.MinValue)
+            {
+                DateTime thirtyDaysAgo = DateTime.Today.AddDays(-30);
+                DateTime t = Convert.ToDateTime(value);
+                if (t < thirtyDaysAgo || t > DateTime.Today)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+    }
+}
